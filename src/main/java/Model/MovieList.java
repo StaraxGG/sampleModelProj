6c97ba9fe67a1fc71ac93b8f7cafc04c19008233
Yasin Tatar <ytatar9@googlemail.com>
2019-02-10 20:08:09 +0100

    added interface MovieList.java

--- src/main/java/Model/MovieList.java
+++ src/main/java/Model/MovieList.java
@@ -1,5 +1,7 @@
 package Model;
 
+import java.util.List;
+
 /**
  * An implementation of MovieList
  * in sample-model-project
@@ -16,7 +18,39 @@ public interface MovieList {
 
     /* ---------------------------------------- Methods ------------------------------------------------------------- */
 
+    /**
+     * adds the movie to this list
+     * @param movie
+     * @return boolean success
+     */
+    boolean addMovie(Movie movie);
+
+    /**
+     * deletes the movie from the list
+     * @param movie
+     * @return boolean success
+     */
+    boolean deleteMovie(Movie movie);
+
     /* ---------------------------------------- S/Getters ----------------------------------------------------------- */
+
+    /**
+     * returns the id of this movielist
+     * @return Long
+     */
+    Long getId();
+
+    /**
+     * get the name for this movielist
+     * @return String
+     */
+    String getName();
+
+    /**
+     * returns the movies of this movielist
+     * @return List
+     */
+    List<Movie> getMovies();
 
 }
 
