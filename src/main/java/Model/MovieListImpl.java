f53a64527764b18e606668314d09e598d7ffc353
Yasin Tatar <ytatar9@googlemail.com>
2019-02-10 22:24:42 +0100

    added movielistimpl class

---
+++ src/main/java/Model/MovieListImpl.java
@@ -0,0 +1,58 @@
+package Model;
+
+import java.util.List;
+
+/**
+ * An implementation of MovieListImpl
+ * in samplemodelproject
+ *
+ * @author ytatar
+ * @version 1.0
+ * @since 2019-Feb-10
+ */
+public class MovieListImpl implements MovieList {
+
+    //TODO: Christian Warken
+    /*
+    Implement all the methods below
+     */
+
+    /* ---------------------------------------- Main ---------------------------------------------------------------- */
+
+    /* ---------------------------------------- Attributes ---------------------------------------------------------- */
+
+    /* ---------------------------------------- Constants ----------------------------------------------------------- */
+
+    /* ---------------------------------------- Constructors -------------------------------------------------------- */
+
+    /* ---------------------------------------- Methods ------------------------------------------------------------- */
+
+    @Override
+    public boolean addMovie(Movie movie) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteMovie(Movie movie) {
+        return false;
+    }
+
+    @Override
+    public Long getId() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public List<Movie> getMovies() {
+        return null;
+    }
+
+    /* ---------------------------------------- S/Getters ----------------------------------------------------------- */
+
+}
+
