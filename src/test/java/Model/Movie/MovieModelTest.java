61fd13949dc3de492605bf86958f9c2b0bfb866a
Yasin Tatar <ytatar9@googlemail.com>
2019-02-22 11:52:09 +0100

    added MovieModelTEst

---
+++ src/test/java/Model/Movie/MovieModelTest.java
@@ -0,0 +1,50 @@
+package Model.Movie;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MovieModelTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void parseTmdbMovie() {
+    }
+
+    @Test
+    public void addMovie() {
+    }
+
+    @Test
+    public void deleteMovie() {
+    }
+
+    @Test
+    public void getMovie() {
+    }
+
+    @Test
+    public void getTmdbMovie() {
+    }
+
+    @Test
+    public void getTmdbMovies() {
+    }
+
+    @Test
+    public void getSimilarMovies() {
+    }
+}
