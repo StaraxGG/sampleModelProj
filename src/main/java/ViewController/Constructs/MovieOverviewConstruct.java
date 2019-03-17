697397fedb17e8fa1f6e0f51f731b5411a9bc17d
StaraxGG <nicolasklein1998@gmail.com>
2019-03-17 16:33:23 +0100

    Added base for MovieOverview

--- src/main/java/ViewController/Constructs/MovieOverviewConstruct.java
+++ src/main/java/ViewController/Constructs/MovieOverviewConstruct.java
@@ -2,12 +2,18 @@ package ViewController.Constructs;
 
 import Model.Movie.Movie;
 import Model.Movie.MoviePosterSize;
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 import java.io.IOException;
 
@@ -19,7 +25,7 @@ import java.io.IOException;
  * @version 1.0
  * @since 2019-Feb-28
  */
-public class MovieOverviewConstruct extends BorderPane {
+public class MovieOverviewConstruct extends StackPane {
 
     /* ---------------------------------------- Main ---------------------------------------------------------------- */
 
@@ -28,10 +34,43 @@ public class MovieOverviewConstruct extends BorderPane {
     /* ---------------------------------------- Attributes ---------------------------------------------------------- */
 
     @FXML
+    private StackPane stackPaneMovieOverview;
+
+    @FXML
+    private Label lblLanguage;
+
+    @FXML
+    private Label lblDirector;
+
+    @FXML
+    private Label lblWriter;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private Label lblRuntime;
+
+    @FXML
+    private Label lblRelease;
+
+    @FXML
+    private Label lblGenres;
+
+    @FXML
+    private Label lblConclusion;
+
+    @FXML
     private ImageView imgPoster;
 
     @FXML
-    private Label lblInfo;
+    private JFXButton btnWatchlist;
+
+    @FXML
+    private JFXButton btnList;
+
+    @FXML
+    private JFXButton btnWatched;
 
     private Movie movie;
 
@@ -58,13 +97,50 @@ public class MovieOverviewConstruct extends BorderPane {
             throw new RuntimeException(exception);
         }
 
-        //todo change back when getPosterURL returns url and not key
-        imgPoster.setImage(new Image(movie.getPosterUrl(MoviePosterSize.W342),
-                200,350,true,true));
+        Image image = new Image(movie.getPosterUrl(MoviePosterSize.W342),342,513,true,true,false);
+        imgPoster.setImage(image);
+
+        Rectangle clip = new Rectangle();
+        clip.setWidth(342);
+        clip.setHeight(513);
+        clip.setArcWidth(20);
+        clip.setArcHeight(20);
+
+        imgPoster.setClip(clip);
+
+        SnapshotParameters parameters = new SnapshotParameters();
+        parameters.setFill(Color.TRANSPARENT);
+        WritableImage image1 = imgPoster.snapshot(parameters,null);
+
+        imgPoster.setClip(null);
+
+        imgPoster.setEffect(new DropShadow(20, Color.BLACK));
+
+        imgPoster.setImage(image1);
+
+        setUpLables(movie);
+
+        System.out.println(movie.getPosterUrl(MoviePosterSize.W342));
+        BackgroundImage myBI= new BackgroundImage(new Image("/graphics/backdrop.jpg",
+                1920,1080,false,true),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+        //then you set to your node
+        stackPaneMovieOverview.setBackground(new Background(myBI));
+
+
+
     }
 
     /* ---------------------------------------- Methods ------------------------------------------------------------- */
 
+    private void setUpLables(Movie movie){
+        lblConclusion.setText(movie.getOverview());
+        lblRelease.setText(movie.getReleaseDate());
+        lblTitle.setText(movie.getTitle());
+        //lblGenres.setText(movie.getGenres().get(0));
+        lblLanguage.setText(movie.getOriginalLanguage());
+    }
 
 
     /* ---------------------------------------- S/Getters ----------------------------------------------------------- */
