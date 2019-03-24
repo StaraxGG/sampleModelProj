a05b2cd3b9147c071e454b0473918aae0cff8deb
JKrieger <jkrieger@htwsaar.de>
2019-03-24 16:25:09 +0100

    add logger to RegisterController

--- src/main/java/ViewController/Controller/LoginController/RegisterController.java
+++ src/main/java/ViewController/Controller/LoginController/RegisterController.java
@@ -1,5 +1,6 @@
 package ViewController.Controller.LoginController;
 
+import Model.Movie.MovieModel;
 import Model.User.User;
 import Model.User.UserImpl;
 import Model.User.UserModel;
@@ -19,6 +20,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -44,9 +47,11 @@ public class RegisterController extends Controller implements Initializable {
     @FXML
     private JFXButton registerButton;
 
+    final static Logger logger = LoggerFactory.getLogger(RegisterController.class);
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        logger.debug("Initializing Component RegisterController");
         this.UMINstance = UserModel.getInstance();
         this.registerButton.setOnMouseClicked(this.registerButtonClicked);
 
@@ -54,22 +59,27 @@ public class RegisterController extends Controller implements Initializable {
     }
 
     private EventHandler<? super MouseEvent> registerButtonClicked = mouseEvent -> {
+        logger.debug("Register button clicked");
         if(this.emailField.getText().isEmpty()){
+            logger.debug("Email Field was empty");
             this.errorLabel.setText("Email can't be empty");
             return;
         }
 
         if(!UserModel.verifyMailAdress(this.emailField.getText())){
+            logger.debug("Invalid email entered. mail: " + this.emailField.getText());
             this.errorLabel.setText("Please enter a valid email!");
             return;
         }
 
         if(this.emailField.getText().isEmpty()){
+            logger.debug("Password field empty");
             this.errorLabel.setText("Password can't be empty");
             return;
         }
 
         if(this.repeatPasswordField.getText().isEmpty()){
+            logger.debug("Password repeat field empty");
             this.errorLabel.setText("Please repeat your password");
             return;
         }
@@ -77,6 +87,7 @@ public class RegisterController extends Controller implements Initializable {
 
 
         if(!this.passwordField.getText().equals(this.repeatPasswordField.getText())){
+            logger.debug("Passwords in passwordField and repeatPasswordField do not match");
             this.errorLabel.setText("Passwords do not match!");
             return;
         }
@@ -84,14 +95,18 @@ public class RegisterController extends Controller implements Initializable {
         User user = new UserImpl(this.emailField.getText(), this.passwordField.getText());
 
         if(!this.UMINstance.register(user)){
-            this.errorLabel.setText("Invalid mail or user already exists");
+            logger.debug("User already exists with name " + user.getUsername());
+            this.errorLabel.setText("User already exists");
         }else{
+            logger.debug("Successfully created User with name " + user.getUsername());
             this.errorLabel.setText("User successfully created! Please login with your new credentials");
             try {
+                logger.debug("Sleeping for 5 Seconds");
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                logger.error(e.getClass().getSimpleName(), e.getMessage());
             }
+            logger.debug("Switching to Login Screen");
             Start.getManager().switchScreenTo(WINDOW_IDENTIFIER.LOGIN);
         }
     };
